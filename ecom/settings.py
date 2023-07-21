9cba4965bd3ea13c9c293e8ba726a9e19edd6819
kriipa <kripathapa2002@gmail.com>
2023-07-21 16:55:46 +0545

    review

--- ecom/settings.py
+++ ecom/settings.py
@@ -132,4 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import *
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -17,6 +19,7 @@ class Homeview(Base):
         self.views['hots'] = Product.objects.filter(labels = 'hot')
         self.views['newprod'] = Product.objects.filter(labels = 'new')
         self.views['ads'] = Ad.objects.all()
+        self.views['count_cart'] = Cart.objects.filter(username = request.user.username, checkout= False).count()
 
         return render(request, 'index.html', self.views)
 
@@ -27,6 +30,7 @@ class CategoryView(Base):
         self.views['categories'] = Category.objects.all()
         self.views['brands'] = Brand.objects.all()
         self.views['sales'] = Product.objects.filter(labels = 'sale')
+        self.views['count_cart'] = Cart.objects.filter(username = request.user.username, checkout= False).count()
 
         return render(request, 'category.html', self.views)
     
@@ -37,6 +41,7 @@ class BrandView(Base):
         self.views['categories'] = Category.objects.all()
         self.views['brands'] = Brand.objects.all()
         self.views['sales'] = Product.objects.filter(labels = 'sale')
+        self.views['count_cart'] = Cart.objects.filter(username = request.user.username, checkout= False).count()
 
         return render(request, 'brand.html', self.views)
     
@@ -46,6 +51,10 @@ class ProductDetail(Base):
         self.views['product_detail'] = Product.objects.filter(slug = slug)
         product_category = Product.objects.get(slug = slug).category_id
         self.views['related_products'] = Product.objects.filter(category_id = product_category)
+        self.views['count_cart'] = Cart.objects.filter(username = request.user.username, checkout= False).count()
+        self.views['count_cart'] = Cart.objects.filter(username = request.user.username, checkout= False).count()
+        self.views['product_reviews'] = ProductReview.objects.filter(slug = slug)
+
         return render(request, 'product-detail.html', self.views)
     
 
@@ -61,8 +70,131 @@ class SearchView(Base):
         self.views['categories'] = Category.objects.all()
         self.views['brands'] = Brand.objects.all()
         self.views['sales'] = Product.objects.filter(labels = 'sale')
+        self.views['count_cart'] = Cart.objects.filter(username = request.user.username, checkout= False).count()
 
         return render(request, 'search.html', self.views)
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        f_name = request.POST['f_name']
+        l_name = request.POST['l_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        
+        if password == cpassword:
+            if User.objects.filter(username = username).exists():
+                messages.error(request, "The username already exists!")
+                return redirect('/signup')
+            
+            elif User.objects.filter(email = email).exists():
+                messages.error(request, "The email is already in use.")
+                return redirect('/signup')
+            
+            else:
+                data = User.objects.create_user(
+                    first_name = f_name,
+                    last_name = l_name,
+                    email = email,
+                    username = username,
+                    password = password
+                )
+                data.save()
+        else:
+            messages.error(request, "Passwords does not match")
+            return redirect('/signup')
     return render(request, 'signup.html')
+
+class CartView(Base):
+    def get(self, request):
+        username = request.user.username
+        self.views['my_cart'] = Cart.objects.filter(username = username)
+        my_cart = Cart.objects.filter(username = username, checkout = False)
+
+        s = 0
+        for i in my_cart:
+            s = s + i.total
+        self.views['all_total'] =s
+        delivery_charge = 100
+        self.views['grand_total'] = s + delivery_charge
+        self.views['count_cart'] = Cart.objects.filter(username = request.user.username, checkout= False).count()
+
+        return render(request, 'cart.html', self.views)
+    
+def add_to_cart(request, slug): 
+    username = request.user.username
+    if Cart.objects.filter(username = username, slug = slug, checkout = False):
+        price = Product.objects.get(slug = slug).price
+        discounted_price = Product.objects.get(slug = slug).discounted_price
+        quantity = Cart.objects.get(slug = slug).quantity
+        quantity = quantity +1
+        if discounted_price > 0:
+            total = discounted_price * quantity
+        else:
+            total = price * quantity
+
+            Cart.objects.filter(username = username, slug = slug, checkout = False).update(
+                quantity = quantity,
+                total = total
+            )
+            return redirect('/cart')
+    else:
+        price = Product.objects.get(slug = slug).price
+        discounted_price = Product.objects.get(slug = slug).discounted_price
+        quantity = 1
+        if discounted_price > 0:
+            total = discounted_price 
+        else:
+            total = price 
+        data = Cart.objects.create(
+            username = username,
+            slug = slug,
+            quantity = quantity,
+            total = total,
+            items = Product.objects.filter(slug = slug)[0]
+        )
+        data.save()
+    return redirect('/cart')
+
+def delete_cart(request, slug):
+    username = request.user.username
+    if Cart.objects.filter(slug = slug, username = username, checkout = False):
+        Cart.objects.filter(slug = slug, username = username, checkout = False).delete()
+    return redirect('/cart')
+
+def reduce_cart(request, slug):
+    username = request.user.username
+    if Cart.objects.filter(username = username, slug = slug, checkout = False):
+        price = Product.objects.get(slug = slug).price
+        discounted_price = Product.objects.get(slug = slug).discounted_price
+        quantity = Cart.objects.get(slug = slug).quantity
+        if quantity > 1:
+            quantity = quantity - 1
+            if discounted_price > 0:
+                total = discounted_price * quantity
+            else:
+                total = price * quantity
+
+            Cart.objects.filter(username = username, slug = slug, checkout = False).update(
+                quantity = quantity,
+                total = total
+            )
+        return redirect('/cart')
+    
+def product_review(request, slug):
+    if Product.objects.filter(slug = slug):
+        if request.method == 'POST':
+            username = request.user.username
+            star = request.POST['star']
+            comments = request.POST['comment']
+            ProductReview.objects.create(
+                username = username,
+                slug = slug,
+                star = star,
+                comments = comments
+            ).save()
+
+    else:
+        return redirect(f'/product/{slug}')
+    return redirect(f'/product/{slug}')
